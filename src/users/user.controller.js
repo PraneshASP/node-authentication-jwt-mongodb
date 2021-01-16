624f352c9929a233546074ee27533a9bd18dd29e
PraneshASP <praneshas2000@gmail.com>
2021-01-16 17:22:04 +0530

    updated code

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -333,23 +333,3 @@ exports.ReferredAccounts = async (req, res) => {
     });
   }
 };
-
-exports.Logout = async (req, res) => {
-  try {
-    const { id } = req.decoded;
-
-    let user = await User.findOne({ userId: id });
-
-    user.accessToken = "";
-
-    await user.save();
-
-    return res.send({ success: true, message: "User Logged out" });
-  } catch (error) {
-    console.error("user-logout-error", error);
-    return res.stat(500).json({
-      error: true,
-      message: error.message,
-    });
-  }
-};
